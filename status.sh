1a10469dc4750384a42898248154474bacab7787
John <jryland@gmail.com>
2021-08-30 10:55:57 +1000

    attempt to better print branch name of submodule

--- status.sh
+++ status.sh
@@ -4,7 +4,7 @@ function status
 {
   pushd ${1} > /dev/null
   URL=$(git config --get remote.origin.url)
-  BRANCH=$(git rev-parse --abbrev-ref HEAD)
+  BRANCH=$(git branch | tail -n 1) # rev-parse --abbrev-ref HEAD)
   printf '%-50s %-12s %-40s\n' "$(pwd)" "${BRANCH}" "${URL}"
   git status -s
   popd > /dev/null
@@ -17,4 +17,6 @@ do
     status ${DIR}
   fi
 done
+
+status .
 
