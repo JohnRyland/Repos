25bfa3259f9a57540b2382b9540ddd8593d4fd47
John Ryland <jryland@xiaofrog.com>
2022-04-08 22:14:39 +1000

    test update script ideas

--- update.sh
+++ update.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
-git submodule update --remote --recursive
-git pull
+git submodule update --init --remote --no-fetch --recursive
+
+function update
+{
+  pushd ${1} > /dev/null
+  BRANCH=$(git branch -r --points-at HEAD | head -n 1 | cut -d '>' -f 2 | cut -d '/' -f 2)
+  # git checkout -b ${BRANCH} --track origin/${BRANCH}
+  git branch -u origin/${BRANCH} ${BRANCH}
+  git fetch
+  git pull origin ${BRANCH}
+  popd > /dev/null
+}
+
+for DIR in *
+do
+  if [ -f "${DIR}/.git" ]
+  then
+    update "${DIR}"
+  fi
+done
+
+update .
 
